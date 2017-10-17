dec140f1b4c567d4fcd9716b29657cea6829341c
johnmboudreaux <jhnbdrx@gmail.com>
2017-10-17 14:42:06 -0600

    built file structure

--- routes/queries.js
+++ routes/queries.js
@@ -9,63 +9,43 @@ var connectionString = 'postgres://localhost:5432/users';
 var db = pgp(connectionString);
 
 function getAllUsers(req, res, next) {
-  db.any('select * from users')
-    .then(function(data) {
-      res.status(200).json({
-        status: 'success',
-        data: data,
-        message: 'Retrieved All Users'
-      });
-    }).catch(function(err) {
-      return next(err)
+  db.any('select * from users').then(function(data) {
+    res.status(200).json({status: 'success', data: data, message: 'Retrieved All Users'});
+  }).catch(function(err) {
+    return next(err)
   });
 }
 
 function signIn(req, res, next) {
-  db.one('select * from users where email=${email} and password=${password}', req.body)
-  .then(function (data) {
-  res.status(200)
-    .json({
-      status: 'success',
-      data: data,
-      message: 'Retrieved ONE User'
-    });
-  })
-  .catch(function (err) {
+  db.one(`select * from users where email=${email} and password=${password}`, req.body).then(function(data) {
+    res.status(200).json({status: 'success', data: data, message: 'Retrieved ONE User'});
+  }).catch(function(err) {
     return next(err);
   });
 }
 
 function createUser(req, res, next) {
   req.body.email = req.body.email.toLowerCase();
-  db.one('insert into users(name, password, email)' + 'values(${name}, ${password}, ${email}) returning id', req.body).then(function(data) {
-    res.status(200).json({ status: 'success', message: "New user created", id: data.id});
+  db.one('insert into users(name, password, email)' + `values(${name}, ${password}, ${email}) returning id`, req.body).then(function(data) {
+    res.status(200).json({status: 'success', message: "New user created", id: data.id});
   }).catch(function(err) {
     res.status(500).json({error: err.detail});
-  })
+  });
 }
 
 function addFavorite(req, res, next) {
-  db.one('insert into favorites(movie_id, user_id, title, poster_path, release_date, vote_average, overview)' +
-  'values(${movie_id}, ${user_id}, ${title}, ${poster_path}, ${release_date}, ${vote_average}, ${overview}) returning id', req.body)
-  .then(function(data) {
-    res.status(200).json({ status: 'success', message: "Movie was added to favorites", id: data.id});
+  db.one(`insert into favorites(movie_id, user_id, title, poster_path, release_date, vote_average, overview)` + `values(${movie_id}, ${user_id}, ${title}, ${poster_path}, ${release_date}, ${vote_average}, ${overview}) returning id`, req.body).then(function(data) {
+    res.status(200).json({status: 'success', message: "Movie was added to favorites", id: data.id});
   }).catch(function(err) {
     next(err);
-  })
+  });
 }
 
 function getAllFavorites(req, res, next) {
   var user_id = parseInt(req.params.id);
-  db.any('select * from favorites where user_id=$1', user_id)
-  .then(function(data) {
-    res.status(200).json({
-      status: 'success',
-      data: data,
-      message: 'Retrieved All favorites'
-    });
-  })
-  .catch(function(err) {
+  db.any('select * from favorites where user_id=$1', user_id).then(function(data) {
+    res.status(200).json({status: 'success', data: data, message: 'Retrieved All favorites'});
+  }).catch(function(err) {
     return next(err);
   });
 }
@@ -74,12 +54,10 @@ function deleteFavorite(req, res, next) {
   var movie_id = parseInt(req.params.movie_id);
   var user_id = parseInt(req.params.id);
   db.result('delete from favorites where user_id = $1 and movie_id = $2', [user_id, movie_id]).then(function(result) {
-    res.status(200)
-    .json({status: 'success', message: `${result.rowCount} row was deleted.`})
-  })
-  .catch(function(err) {
+    res.status(200).json({status: 'success', message: `${result.rowCount} row was deleted.`})
+  }).catch(function(err) {
     return next(err);
-  })
+  });
 }
 
 module.exports = {
