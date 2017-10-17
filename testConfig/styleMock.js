5a25e130423219a1eeb24b9e61aa979a206e6b62
Alex Banister <alexbanister@gmail.com>
2017-10-17 15:10:45 -0600

    add mock data

--- testConfig/styleMock.js
+++ testConfig/styleMock.js
@@ -1,1 +1,28 @@
-module.exports = {};
+module.exports = {
+  "results": [
+    {
+      "vote_count": 881,
+      "id": 335984,
+      "video": false,
+      "vote_average": 7.8,
+      "title": "Blade Runner 2049",
+      "popularity": 452.191852,
+      "poster_path": "/aMpyrCizvSdc0UIMblJ1srVgAEF.jpg",
+      "original_language": "en",
+      "original_title": "Blade Runner 2049",
+      "genre_ids": [
+        28, 9648, 878, 53
+      ],
+      "backdrop_path": "/mVr0UiqyltcfqxbAUcLl9zWL8ah.jpg",
+      "adult": false,
+      "overview": "Thirty years after the events of the first film, a new blade runner, LAPD Officer K, unearths a long-buried secret that has the potential to plunge what's left of society into chaos. K's discovery leads him on a quest to find Rick Deckard, a former LAPD blade runner who has been missing for 30 years.",
+      "release_date": "2017-10-04"
+    }],
+  "page": 1,
+  "total_results": 407,
+  "dates": {
+    "maximum": "2017-11-10",
+    "minimum": "2017-10-16"
+  },
+  "total_pages": 21
+};
