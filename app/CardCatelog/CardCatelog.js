a7dbc33cb3095e5fcfbb7e636bc8fc8ff12698b5
johnmboudreaux <jhnbdrx@gmail.com>
2017-10-17 17:36:22 -0600

    got movies displaying to the dom

--- app/CardCatelog/CardCatelog.js
+++ app/CardCatelog/CardCatelog.js
@@ -2,18 +2,31 @@ import { connect } from 'react-redux';
 import { addTodo } from './CardCatelogActions';
 import React, { Component } from 'react';
 import { fetchRecentMovies } from '../API/movieDatabase';
+import Card from '../Card/Card'
 
 export default class CardCatelog extends Component {
+  constructor(){
+    super();
+    this.state = {
+      recentMovies: []
+    };
+  }
 
   async componentDidMount() {
+    //set state
     const recentMovies = await fetchRecentMovies();
+    this.setState({
+      recentMovies: recentMovies
+    });
   }
 
   render() {
+    const allMovies = this.state.recentMovies.map( (movie) => {
+      return (<Card key={movie.id} movie={movie} />);
+    });
     return (
       <div>
-        <h1>Movie Watcher</h1>
-        <h1>Movie Watcher</h1>
+        {allMovies}
       </div>
     );
   }
