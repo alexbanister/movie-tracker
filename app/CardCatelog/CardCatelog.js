4edc540e42ab8ff95bb162cf5bf46f5d640fabc8
Alex Banister <alexbanister@gmail.com>
2017-10-19 13:01:13 -0600

    header styles

--- app/CardCatelog/CardCatelog.js
+++ app/CardCatelog/CardCatelog.js
@@ -19,7 +19,6 @@ class CardCatelog extends Component {
       accessibility: true,
       arrows: true,
       variableWidth: false,
-      speed: 500,
       slidesToShow: 4,
       slidesToScroll: 1,
       focusOnSelect: true,
